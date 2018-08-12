36e784d6721234418d10470c8e9c14ec69ef31fb
Marcos Ostos <marcos.ostos@gmail.com>
2018-08-12 15:36:10 +0200

    Development, add watch and webpack-dev-server

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ function component() {
     let element = document.createElement('div');
     var btn = document.createElement('button');
   
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.innerHTML = _.join(['Hola', 'webpack'], ' ');
     btn.onclick = printMe;
 
     element.appendChild(btn);
